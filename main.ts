3772e3287a4f3e49bb219d64a6944a71deae50ed
MahammadHashim786 <161362132+MahammadHashim786@users.noreply.github.com>
2024-03-26 14:06:09 +0500

    New calculator
    my fisrt calculator program

---
+++ main.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+import chalk from "chalk";
+
+const answer :{
+    numberOne: number,
+    numberTwo: number,
+    operator: string,
+} = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Enter your First Number",
+    },{
+        type: "number",
+        name: "numberTwo",
+        message: "Enter your second Number",
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Enter your operator",
+        choices:["+","-","*","/"]
+    }
+])
+const { numberOne, numberTwo, operator} = answer;
+
+let result
+switch(operator){
+    case"+" : result = numberOne + numberTwo;
+    break;
+    case"-" : result = numberOne - numberTwo;
+    break;
+    case"*" : result = numberOne * numberTwo;
+    break;
+    case"/" : result = numberOne / numberTwo;
+    break;
+    default:
+        console.log("invalid operator");
+
+}
+console.log(chalk.yellow(`${numberOne} ${numberTwo} ${operator} = ${result}`));
+
